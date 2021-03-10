3f0aec0d31064525b1121de097e280c77bda054e
carasilo <carasilo.daviluis@gmail.com>
2021-03-09 21:55:31 -0300

    Alteracoes aula 090320221

--- foto-ia/src/App.js
+++ foto-ia/src/App.js
@@ -2,13 +2,16 @@ import React, { useState } from "react";
 import { ReactComponent as Robot } from '../src/images/robot.svg'
 import './App.css'
 import Carregando from './images/Spinner-1s-98px.gif'
+import {MdClear,MdMoveToInbox} from "react-icons/md"
 
 //JSX Java Script Extension
 function App() {
 
   const [pessoas, setPessoas] = useState([]);
   const [estaCarregando, setEstaCarregando] = useState(false);
-
+  const [idade, setIdade] = useState('');
+  const [etnia, setEtnia] = useState('');
+  const estiloIcone = {background: 'red',fontSize: '1.5em'}
   function ListaPessoas() {
     const listagemPessoas = pessoas.map((pessoa) =>
       <img key={pessoa.id} src={pessoa.urls[4][512]} title="Gerada por IA"
@@ -20,11 +23,15 @@ function App() {
       </div>
     )
   }
-  
+
 
   async function obterFoto() {
     setEstaCarregando(true);
-    let url = "https://api.generated.photos/api/v1/faces?api_key=dO6zZQ35opwSFsIGdm9iSw"
+    let chaveAPI = process.env.REACT_APP_APIKEY;
+    const filtraEtnia = etnia.length > 0 ? `&ethnicity=${etnia}`  : ``;
+    const filtraIdade = idade.length > 0 ? `&age=${idade}`  : ``;
+
+    let url = `https://api.generated.photos/api/v1/faces?api_key=${chaveAPI}${filtraEtnia}${filtraIdade}&order_by=random`
     await fetch(url)
       .then(response => response.json())
       .then(data => {
@@ -34,7 +41,7 @@ function App() {
       .catch(function (error) {
         console.error("Houve um problema na requisição " + error.message);
       })
-      setEstaCarregando(false);
+    setEstaCarregando(false);
   }
 
   return (
@@ -45,10 +52,38 @@ function App() {
       { estaCarregando &&
         <img src={Carregando} title="Aguarde..." alt="Imagem carregando" />
       }
-      <ListaPessoas />
-      <button type='button' onClick={obterFoto}>
-        Obter Imagens
+      <div className="linha">
+        <ListaPessoas />
+      </div>
+      <div className="linha">
+        <label>Idade:</label>
+        <select onChange={e => setIdade(e.target.value)}>
+          <option value="">Todas</option>
+          <option value="infant">Infantil</option>
+          <option value="child">Criança</option>
+          <option value="young-adult">Jovem</option>
+          <option value="adult">Adulto</option>
+          <option value="elderly">Idoso</option>
+        </select>
+        <label>Etnia:</label>
+        <select  onChange={e => setEtnia(e.target.value)}>
+          <option value="">Todas</option>
+          <option value="white">Branca</option>
+          <option value="latino">Latina</option>
+          <option value="asian">Asiática</option>
+          <option value="black">Negra</option>
+        </select>       
+      </div>
+      <div className="linha">
+        <button type='button' onClick={obterFoto}>
+        <MdMoveToInbox/> Obter Imagens
       </button>
+        {pessoas.length > 0 &&
+          <button type="button" onClick={() => setPessoas([])}>
+            <MdClear style={estiloIcone}/> Limpar Imagens
+         </button>
+        }
+      </div>
     </div>
   )
 }
